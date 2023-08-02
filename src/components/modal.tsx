33f97a79627b2313a21679952b4bf0fe6b860ffc
eks <kimzer@vista.aero>
2023-08-02 16:48:00 +0800

    feat. modal

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -2,13 +2,18 @@ import { Fragment, useRef, useState } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
 import { ExclamationTriangleIcon } from '@heroicons/react/24/outline'
 
-export default function Example() {
+interface ModalProps {
+  item: any; // replace 'any' with the actual type of 'item'
+  onClose: () => void;
+}
+
+export default function Modal({ item, onClose }: ModalProps) {
   const [open, setOpen] = useState(true)
 
   const cancelButtonRef = useRef(null)
 
   return (
-    <Transition.Root show={open} as={Fragment}>
+    <Transition.Root show={true} as={Fragment}>
       <Dialog as="div" className="relative z-10" initialFocus={cancelButtonRef} onClose={setOpen}>
         <Transition.Child
           as={Fragment}
@@ -56,14 +61,14 @@ export default function Example() {
                   <button
                     type="button"
                     className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
-                    onClick={() => setOpen(false)}
+                    onClick={onClose} // {() => setOpen(false)}
                   >
                     Deactivate
                   </button>
                   <button
                     type="button"
                     className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
-                    onClick={() => setOpen(false)}
+                    onClick={onClose} // {() => setOpen(false)}
                     ref={cancelButtonRef}
                   >
                     Cancel
