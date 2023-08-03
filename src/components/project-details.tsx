75e2b9cf53c03fd58125a66f26e835df4b704643
eks <kimzer@vista.aero>
2023-08-04 01:20:27 +0800

    update
    1. 对表格数据按id进行排序；
    2. 调整project-details模态框的按钮位置及自动焦点

--- src/components/project-details.tsx
+++ src/components/project-details.tsx
@@ -1,6 +1,5 @@
-import { useState, useRef } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import { Dialog } from '@headlessui/react'
-import { ExclamationTriangleIcon } from '@heroicons/react/24/outline'
 import Modal from './modal'
 
 interface ProjectDetailsProps {
@@ -10,7 +9,13 @@ interface ProjectDetailsProps {
 
 export default function ProjectDetails({ item, onClose }: ProjectDetailsProps) {
   const [open, setOpen] = useState(true)
-  const closeButtonRef = useRef(null)
+  const closeButtonRef = useRef<HTMLButtonElement>(null)
+
+  useEffect(() => {
+    if (closeButtonRef.current) {
+      closeButtonRef.current.focus();
+    }
+  }, []);
 
   const handleClose = () => {
     setOpen(false);
@@ -30,27 +35,30 @@ export default function ProjectDetails({ item, onClose }: ProjectDetailsProps) {
                 <p className="text-sm text-gray-500">
                   测试文本 行1
                   测试文本 行2
-                  测试文本 行3  
+                  测试文本 行3
+                  测试文本 行4
+                  测试文本 行5
+                  测试文本 行6
                 </p>
               </div>
             </div>
           </div>
         </div>
-        <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+        <div className="bg-white px-4 py-3 sm:justify-end sm:flex sm:flex-row sm:px-6">
           <button
             type="button"
-            className="inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-mono text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:ml-3 sm:w-auto"
+            className="sm:mr-3 inline-flex justify-center w-full sm:w-auto px-3 py-2 rounded-md text-sm font-mono shadow-sm text-white bg-blue-600 hover:bg-blue-500"
+            onClick={onClose}
+          >
+            编辑
+          </button>
+          <button
+            type="button"
+            className="mt-3 sm:mt-0 inline-flex justify-center w-full sm:w-auto px-3 py-2 rounded-md text-sm font-mono shadow-sm ring-1 ring-inset ring-gray-300 text-gray-900 bg-white hover:bg-gray-50"
             onClick={onClose}
             ref={closeButtonRef}
           >
             关闭
-          </button>
-          <button
-            type="button"
-            className="mt-3 inline-flex w-full justify-center rounded-md bg-blue-600 px-3 py-2 text-sm font-mono text-white shadow-sm hover:bg-blue-500 sm:mt-0 sm:w-auto"
-            onClick={onClose}
-          >
-            编辑
           </button>
         </div>
       </Dialog.Panel>
